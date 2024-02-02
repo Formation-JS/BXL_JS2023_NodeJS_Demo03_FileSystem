d1fb2e7fbe0402f1bd61c701b8a2e0819aa5bb9a
Aurélien <aurelien.strimelle@bstorm.be>
2024-02-02 12:14:49 +0100

    Demo 3 - Module FileSystem

---
+++ services/guest2.service.js
@@ -0,0 +1,15 @@
+
+async function addGuest({firstname, lastname}) {
+
+}
+
+async function getGuests() {
+
+}
+
+
+const guestService = {
+    addGuest,
+    getGuests
+};
+module.exports = guestService;
